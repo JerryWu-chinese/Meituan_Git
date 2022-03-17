eda9de8b76537fc350cefc7528e3a1f22e3c1f33
JerryWu-chinese <w1637090337hz@gmail.com>
2022-03-17 17:18:42 +0800

    first-commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          xiangyi <code>src/App.js</code> and save to reload.
+          JerryWu <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
